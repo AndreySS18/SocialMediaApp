6683e04db5e87eaca5a1678cd2607733ec92f91f
AndreySS18 <andrey.sychev.2003@mail.ru>
2024-12-20 20:53:58 +0300

    MVP without auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,62 @@
-import React, { useEffect } from 'react';
-import { Container, AppBar, Typography, Grow, ThemeProvider, createTheme} from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Container, AppBar, Typography, Grow, ThemeProvider, createTheme } from '@mui/material';
 import { useDispatch } from 'react-redux';
 
 import { getPosts } from './actions/posts';
-import { Grid2 } from '@mui/material';
+import { Grid } from '@mui/material';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import memories from './images/memories.png';
 import useStyles from './styles';
 
 const theme = createTheme({
-  spacing: 4, // Настроить отступы по необходимости
+  spacing: 5, // Настроить отступы по необходимости
 });
 
 const App = () => {
-  const classes = useStyles();  // Предполагается, что useStyles правильно работает
+  const [currentId, setCurrentId] = useState(null);
+  const classes = useStyles();
   const dispatch = useDispatch();
 
-  // Получаем посты при монтировании компонента
   useEffect(() => {
     dispatch(getPosts());
-  }, [dispatch]);
+  }, [currentId, dispatch]);
 
   return (
-    <ThemeProvider theme={theme}> {/* Оборачиваем приложение в ThemeProvider */}
+    <ThemeProvider theme={theme}> 
       <Container maxWidth="lg">
-        <AppBar className={classes.appBar} position="static" color="inherit">
-          <Grid2 container alignItems="center" justifyContent="space-between">
-            <Grid2 item="true">
-              <Typography className={classes.heading} variant="h2">
+        <AppBar className={classes.appBar} position="static" color="inherit" style={{ borderRadius: 10, padding: '10px 20px' }}>
+          <Grid container justifyContent="center" alignItems="center" spacing={2}>
+            <Grid item>
+              <img className={classes.image} src={memories} alt="icon" height="60" />
+            </Grid>
+            <Grid item>
+              <Typography className={classes.heading} variant="h2" align="center">
                 Memories
               </Typography>
-            </Grid2>
-            <Grid2 item="true">
-              <img className={classes.image} src={memories} alt="icon" height="60" />
-            </Grid2>
-          </Grid2>
+            </Grid>
+          </Grid>
         </AppBar>
+
         <Grow in>
           <Container>
-            <Grid2 container justifyContent="space-between" alignItems="stretch" spacing={3}>
-              <Grid2 item xs={12} sm={7}>
-                <Posts />
-              </Grid2>
-              <Grid2 item xs={12} sm={4}>
-                <Form />
-              </Grid2>
-            </Grid2>
+            <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
+              <Grid item xs={12} md={8}>
+                <div style={{ borderRadius: 0, overflow: 'hidden' }}>
+                  <Posts setCurrentId={setCurrentId} />
+                </div>
+              </Grid>
+
+              <Grid item xs={12} md={4}>
+                <div style={{ borderRadius: 10, padding: 10 }}>
+                  <Form currentId={currentId} setCurrentId={setCurrentId} />
+                </div>
+              </Grid>
+            </Grid>
           </Container>
         </Grow>
       </Container>
-    </ThemeProvider>  
+    </ThemeProvider>
   );
 };
 
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,22 +1,27 @@
 import React from "react";
+import { Grid2, CircularProgress } from '@mui/material';
 import { useSelector } from "react-redux";
 
 import Post from './Post/Post'
 import useStyles from './styles';
 
 
-const Posts = () => {
+const Posts = ({setCurrentId}) => {
     const classes = useStyles();
     const posts = useSelector((state) => state.posts);
 
     console.log(posts);
 
     return (
-        <>
-            <h1 className={classes.smMargin}>POSTS</h1>
-            <Post />
-            <Post />
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid2 className={classes.container} container alignItems="stretch" spacing={3}>
+                {posts.map(((post) =>
+                    <Grid2 key={post.id} item xs={12} sm={6}>
+                        <Post post={post} setCurrentId={setCurrentId}/>
+                    </Grid2>
+                ))}
+            </Grid2>
+        )
     );
 }
 

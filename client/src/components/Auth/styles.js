fd6c48263196447808c2ead74a5824fad16d7630
maratmarat22 <akhmyarovmarat@mail.ru>
2024-12-22 18:32:16 +0300

    Auth server

--- client/src/components/Auth/styles.js
+++ client/src/components/Auth/styles.js
@@ -8,23 +8,25 @@ export default makeStyles((theme) => ({
     alignItems: 'center',
     padding: theme.spacing(2),
   },
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-    },
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: '100%',
     marginTop: theme.spacing(3),
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
+    backgroundColor: '#3f51b5', 
+    '&:hover': {
+      backgroundColor: '#2c387e', 
+    },
   },
   googleButton: {
     marginBottom: theme.spacing(2),
+  },
+  fieldSpacing: {
+    marginBottom: theme.spacing(2), 
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: '#f44336', 
   },
 }));
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@mui/material';
-
+import { useDispatch } from 'react-redux';
 import useStyles from './styles';
 import memories from '../../images/memories.png';
 
 const Navbar = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+    const history = useNavigate();
+    const location = useLocation();
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const logout = () => {
+      dispatch({type : 'LOGOUT'});
 
-    const user = null;
+      history.push('/');
+
+      setUser(null);
+    }
+
+    useEffect(() => {
+      const token = user?.token;
+
+      setUser(JSON.parse(localStorage.getItem('profile')));
+    }, [location]);
 
     return (
         <AppBar className={classes.appBar} position="static" color="inherit" style={{ borderRadius: 10, padding: '10px 20px', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
@@ -23,7 +38,7 @@ const Navbar = () => {
                     <div className={classes.profile}>
                         <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageYrl}>{user.result.name.charAt(0)}</Avatar>
                         <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
-                        <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+                        <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
                     </div>
                 ) : (
                     <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>

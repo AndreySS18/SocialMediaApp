c83846a3b6c75a8fdbb42ed222128bd279c164ae
maratmarat22 <akhmyarovmarat@mail.ru>
2024-12-22 14:17:44 +0300

    add Auth Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Container, ThemeProvider, createTheme } from '@mui/material';
 
 import Navbar from './components/Navbar/Navbar';
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {AppBar, Typography} from '@mui/material';
-import { Grid } from '@mui/material';
+import { AppBar, Avatar, Button, Toolbar, Typography } from '@mui/material';
 
 import useStyles from './styles';
 import memories from '../../images/memories.png';
@@ -9,23 +8,29 @@ import memories from '../../images/memories.png';
 const Navbar = () => {
     const classes = useStyles();
 
-    return (
-    <AppBar className={classes.appBar} position="static" color="inherit" style={{ borderRadius: 10, padding: '10px 20px' }}>
-        <div className={classes.brandContainer}>
+    const user = null;
 
-        </div>
-    <Grid container justifyContent="center" alignItems="center" spacing={2}>
-      <Grid item>
-        <img className={classes.image} src={memories} alt="icon" height="60" />
-      </Grid>
-      <Grid item>
-        <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">
-          Memories
-        </Typography>
-      </Grid>
-    </Grid>
-    </AppBar>
-  );
+    return (
+        <AppBar className={classes.appBar} position="static" color="inherit" style={{ borderRadius: 10, padding: '10px 20px', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+            <div className={classes.brandContainer} style={{ display: 'flex', alignItems: 'center' }}>
+                <img className={classes.image} src={memories} alt="icon" height="60" />
+                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center" style={{ marginLeft: '10px' }}>
+                    Memories
+                </Typography>
+            </div>
+            <Toolbar className={classes.toolbar} style={{ marginLeft: 'auto' }}>
+                {user ? (
+                    <div className={classes.profile}>
+                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageYrl}>{user.result.name.charAt(0)}</Avatar>
+                        <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+                        <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+                    </div>
+                ) : (
+                    <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+                )}
+            </Toolbar>
+        </AppBar>
+    );
 };
 
 export default Navbar;

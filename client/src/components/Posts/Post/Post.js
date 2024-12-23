5ead0506ccde9b72c639eed24e19fbad4e510ee3
maratmarat22 <akhmyarovmarat@mail.ru>
2024-12-23 12:20:58 +0300

    Last changes

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -32,14 +32,16 @@ const Post = ({post, setCurrentId}) => {
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
             <div className={classes.overlay}>
-                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="h6">{post.name}</Typography>
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
+            {(user?.result?.name === post?.name || user?.result?.name === post?.name) && (
             <div className={classes.overlay2}>
                 <Button style={{color: 'white'}} size="small" onClick={()=>setCurrentId(post._id)}>
                     <MoreHorizIcon fontSize="default" />
                 </Button>
             </div>
+            )}
             <div className={classes.details}>
                 <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
             </div>
@@ -51,10 +53,12 @@ const Post = ({post, setCurrentId}) => {
                 <Button size="small" color="primary" disabled={!user?.result} onClick={() => dispatch(likePost(post._id))}>
                     <Likes />
                 </Button>
+                {(user?.result?.name === post?.name || user?.result?.name === post?.name) && (
                     <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize="small" />
                     Delete
                 </Button>
+                )}
             </CardActions>
         </Card>
     );
--- client/src/components/Posts/Post/styles.js
+++ client/src/components/Posts/Post/styles.js
@@ -1,52 +1,42 @@
 import { makeStyles } from '@mui/styles';
 
-export default makeStyles({
-    media: {
-      height: 0,
-      paddingTop: '56.25%',
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',
-      backgroundBlendMode: 'darken',
-    },
-    border: {
-      border: 'solid',
-    },
-    fullHeightCard: {
-      height: '100%',
-    },
-    card: {
+export default makeStyles(() => ({
+  card: {
+      width: '300px', // фиксированная ширина
+      margin: '20px',
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'space-between',
-      borderRadius: '15px',
-      height: '100%',
       position: 'relative',
-    },
-    overlay: {
+      minHeight: '400px', // одинаковая высота
+  },
+  media: {
+      height: '150px',
+      backgroundSize: 'cover',
+  },
+  overlay: {
       position: 'absolute',
-      top: '20px',
-      left: '20px',
+      top: '10px',
+      left: '10px',
       color: 'white',
-    },
-    overlay2: {
+  },
+  overlay2: {
       position: 'absolute',
-      top: '20px',
-      right: '20px',
+      top: '10px',
+      right: '10px',
       color: 'white',
-    },
-    grid: {
-      display: 'flex',
-    },
-    details: {
+  },
+  details: {
       display: 'flex',
       justifyContent: 'space-between',
       margin: '20px',
-    },
-    title: {
+  },
+  title: {
       padding: '0 16px',
-    },
-    cardActions: {
+  },
+  cardActions: {
       padding: '0 16px 8px 16px',
       display: 'flex',
       justifyContent: 'space-between',
-    },
-  });
+  },
+}));
